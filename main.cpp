00dcb9ce64cecef3d1e9b72a6bf97797efc5b664
gonzalojosehernandez <gonzalojosehernandez@db5d8f0b-8f9a-d208-9591-873e7d5d0788>
2015-02-23 22:50:20 +0000

    Implementation of links to sonar.udenar.edu.co/*

--- main.cpp
+++ main.cpp
@@ -2,4 +2,16 @@
 
 PROGRAM START
 
+for (int i=100; i<450; i+=50)
+{
+    circle(i,100,50);
+}
+
+color(BLUE);
+
+for (int i=50,j=200; i<400; i+=40,j+=10)
+{
+    rectangle(i,j,i+100,j+100);
+}
+
 END
